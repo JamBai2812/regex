6e82f1b6ea82704e5af172abef1bfc780403607d
Kanwaljit Dilawari <Kanwaljit.Dilawari@softwire.com>
2020-02-04 11:32:22 +0000

    part 1 readfile, parse for email string, return count

---
+++ regex/regex/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            string readFile = ReadFile("sample.txt");
+            EmailCount(readFile);
+
+           
+        }
+        public static string ReadFile(string path)
+        {
+            string readText = File.ReadAllText(path);
+            return readText;
+        }
+
+        public static void EmailCount(string readText)
+        {
+            int counter = 0;
+            
+            for (int i = 0; i < readText.Length - 13; i++)
+            {
+                if (readText.Substring(i, 13) == "@softwire.com")
+                {
+                    counter++;
+                }
+            }
+            Console.Write(counter);
+            
+            
+
+            
+        }
+    }
+}
